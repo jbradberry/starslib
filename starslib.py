419a48c17349a81d8a76572dda8bf793cbf84927
Jeff Bradberry <jeff.bradberry@gmail.com>
2010-05-18 23:18:58 -0400

    Changed to use a generator to split out fields, and use functions instead of objects to wrap the fields.

--- starslib.py
+++ starslib.py
@@ -14,76 +14,6 @@ prime = (3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53,
     601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673,
     677, 683, 691, 701, 709, 719, 727)
 
-class StarsStruct(object):
-    def __init__(self, data):
-        self._data = data
-
-    def __str__(self):
-        return str(struct.unpack("%dB" % len(self._data), self._data))
-
-class BOF(StarsStruct):
-    def __init__(self, data):
-        self._data = data
-        (self.magic,
-         self.uid,
-         self.version,
-         self.turn,
-         ps, f) = struct.unpack("4sI4H", data)
-        self.player = ps & 0x001f
-        self.salt = (ps & 0xffe0)>>5
-        self.ftype = f & 0x00ff
-        self.done = (f & 0x0100)>>8
-        self.inuse = (f & 0x0200)>>9
-        self.multi = (f & 0x0400)>>10
-        self.g_over = (f & 0x0800)>>11
-        self.share = (f & 0x1000)>>12
-        self.other = (f & 0xe000)>>13
-        prng_init(self.share, self.player, self.turn, self.salt, self.uid)
-
-class Field6(StarsStruct):
-    def __init__(self, data):
-        self._data = data
-        L = len(data)//4 + (len(data) % 4 != 0)
-        tmp = struct.unpack("%dI" % L, data + '\x00'*(4*L-len(data)))
-        tmp = [i^prng() for i in tmp]
-        tmp = struct.pack("%dI" % L, *tmp)
-        self.info = struct.unpack("%dB" % 4*L, tmp)
-
-    def __str__(self):
-        return str(self.info)
-
-class Field7(StarsStruct):
-    def __init__(self, data):
-        self._data = data
-        L = len(data)//4 + (len(data) % 4 != 0)
-        tmp = struct.unpack("%dI" % L, data + '\x00'*(4*L-len(data)))
-        tmp = [i^prng() for i in tmp]
-        tmp = struct.pack("%dI" % L, *tmp)
-        self.info = struct.unpack("%dB" % 4*L, tmp)
-        self.stars = self.info[10] + self.info[11]*256
-
-    def __str__(self):
-        return str(self.info)
-
-class StarIdXY(StarsStruct):
-    def __init__(self, data):
-        self._data = data
-        tmp = struct.unpack("I", data)[0]
-        self.x = tmp & 0x000003ff
-        self.y = (tmp & 0x003ffc00)>>10
-        self.ind = (tmp & 0xffc00000)>>22
-
-    def __str__(self):
-        return "%3d: %4d, %6d" % (self.ind, self.x, self.y)
-
-class EOF(StarsStruct):
-    pass
-
-fields = {8: BOF,
-          7: Field7,
-          6: Field6,
-          0: EOF}
-
 def prng():
     global hi, lo
     lo = (0x7fffffab * int(lo / -53668) + 40014 * lo) % (1<<32)
@@ -108,33 +38,62 @@ def prng_init(flag, player, turn, salt, uid):
         burn = prng()
     #print hex(lo), hex(hi)
 
-ind = 0
-def read_struct():
-    global ind, data
-    hdr = struct.unpack("H", data[ind:ind+2])[0]
-    ind += 2
-    stype, size = (hdr & 0xfc00)>>10, hdr & 0x03ff
-    #res = struct.unpack("%dB" % size, data[ind:ind+size])
-    res = fields.get(stype, StarsStruct)(data[ind:ind+size])
-    ind += size
-    return res
-
-def read_field(size):
-    global ind, data
-    tmp = data[ind:ind+size]
-    ind += size
+def crypt(lst):
+    L = len(lst)
+    if L % 4 != 0: # not sure this is the right thing
+        lst = list(lst) + [0] * (4 - L%4)
+        L = len(lst)
+    tmp = struct.pack("%dB" % L, *lst)
+    tmp = struct.unpack("%dI" % (L//4), tmp)
+    tmp = [x^prng() for x in tmp]
+    tmp = struct.pack("%dI" % (L//4), *tmp)
+    tmp = struct.unpack("%dB" % L, tmp)
     return tmp
+
+def bof(lst):
+    tmp = struct.pack("16B", *lst)
+    magic, uid, ver, turn, ps, f = struct.unpack("4sI4H", tmp)
+    player = ps & 0x001f
+    salt = (ps & 0xffe0)>>5
+    share = (f & 0x1000)>>12
+    prng_init(share, player, turn, salt, uid)
+    return lst
+
+def ixy(lst):
+    tmp = struct.pack("4B", *lst)
+    tmp = struct.unpack("I", tmp)[0]
+    x = tmp & 0x000003ff
+    y = (tmp & 0x003ffc00)>>10
+    i = (tmp & 0xffc00000)>>22
+    return i, x, y
+
+process = {-1: ixy,
+           6: crypt,
+           7: crypt,
+           8: bof}
+
+def parse(data):
+    index, head = 0, True
+    while index < len(data):
+        if head:
+            hdr = struct.unpack("H", data[index:index+2])[0]
+            stype, size = (hdr & 0xfc00)>>10, hdr & 0x03ff
+            index += 2
+        else:
+            stype, size = -1, 4
+        buf = process.get(stype, lambda x: x)(
+            struct.unpack("%dB" % size, data[index:index+size]))
+        yield (stype, size, buf)
+        index += size
+        if not head:
+            stars -= 1
+            if stars == 0: head = True
+        if stype == 7:
+            head, stars = False, buf[10] + 256*buf[11]
 
 if __name__ == '__main__':
     with open(sys.argv[1], 'rb') as f:
-        data = f.read()
+        data = parse(f.read())
 
-    while True:
-        current = read_struct()
-        print type(current), current
-        if isinstance(current, EOF):
-            break
-        if isinstance(current, Field7):
-            for i in xrange(current.stars):
-                current = StarIdXY(read_field(4))
-                print type(current), current
+    for field in data:
+        print field
