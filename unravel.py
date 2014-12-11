d601ffcc4b7c8447a31cbd2a684ec596e18092fb
Jeff Bradberry <jeff.bradberry@gmail.com>
2014-12-11 16:50:37 -0500

    Added a script to parse and print to output Stars files.

---
+++ unravel.py
@@ -0,0 +1,26 @@
+import sys
+import os
+import re
+
+from starslib import base
+
+starsRE = re.compile('.*\.([hmxr]{1}[0-9]{1,2}|xy|hst)')
+
+foo = base.StarsFile()
+
+fpath = sys.argv[1]
+fpaths = []
+if os.path.isfile(fpath):
+    fpaths = [fpath]
+else:
+    for root, dirs, files in os.walk(fpath):
+        fpaths.extend(os.path.join(root, fname) for fname in files
+                      if starsRE.match(fname))
+
+
+for fpath in fpaths:
+    with open(fpath, 'r') as f:
+        foo.bytes = f.read()
+
+    for S in foo.structs:
+        print S.type, unicode(S)
