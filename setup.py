1a3f148f609b798c765a44b72d97f153ecd9fe81
Jeff Bradberry <jeff.bradberry@gmail.com>
2020-03-19 21:53:46 -0400

    Add a setup.py to make this installable

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import find_packages, setup
+
+
+# Package meta-data.
+NAME = 'starslib'
+DESCRIPTION = "A library for parsing Stars files."
+URL = 'https://github.com/jbradberry/starslib'
+EMAIL = 'jeff.bradberry@gmail.com'
+AUTHOR = 'Jeff Bradberry'
+# REQUIRES_PYTHON = '>=3.6.0'
+VERSION = None
+
+REQUIRED = []
+
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description="",
+    author=AUTHOR,
+    author_email=EMAIL,
+    url=URL,
+    packages=find_packages(exclude=('tests',)),
+    install_requires=REQUIRED,
+    include_package_data=True,
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Games/Entertainment',
+    ],
+)
